0db37e2edb8fa7f31858df28f3aae5c5184944a3
roney ruiz <roneyruizrojas@gmail.com>
2026-01-07 22:14:23 -0600

    feat: setup apollo client config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,26 @@
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { ApolloProvider } from "@apollo/client/react";
 import { PlaygroundPage } from "@Features/Playground/Page";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const root = document.getElementById("root")!;
 
+const link = new HttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+const apollo = new ApolloClient({
+  link, cache: new InMemoryCache(),
+});
+
 createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<PlaygroundPage />} />
-    </Routes>
-  </BrowserRouter>
+  <ApolloProvider client={apollo}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PlaygroundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </ApolloProvider>
 );
