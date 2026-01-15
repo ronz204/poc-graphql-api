74a67b260a4d86c9713a1f7c34bb0d2110e15be2
roney ruiz <roneyruizrojas@gmail.com>
2026-01-14 19:45:39 -0600

    feat: adjust elysia rest example

--- server/src/Entrypoint.ts
+++ server/src/Entrypoint.ts
@@ -1,5 +1,9 @@
-import { Elysia } from "elysia";
+import Elysia from "elysia";
+const app = new Elysia();
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+app.get("/", () => {
+  return { message: "Hello Elysia" };
+});
 
+app.listen(3000);
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
