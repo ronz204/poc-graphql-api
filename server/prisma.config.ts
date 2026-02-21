98f2e752102ce8059d4eaa3b78e9234554b5f362
roney ruiz <roneyruizrojas@gmail.com>
2026-02-21 15:35:28 -0600

    chore(elysia): rename migration and model folder

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/",
   migrations: {
-    path: "prisma/migrations",
+    path: "prisma/Migrations",
   },
   datasource: {
     url: env("POSTGRES_URL"),
